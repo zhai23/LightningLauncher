3a832902902c7f4b1e20cdd7cb5b2837aacbfcd9
threethan <ethanjmedei@gmail.com>
2023-09-12 12:16:54 -0400

    Simplify Shortcut APK

--- LauncherShortcut/app/src/main/java/com/facebook/orca/MainActivity.kt
+++ LauncherShortcut/app/src/main/java/com/facebook/orca/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.facebook.orca
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import java.util.Timer
-import java.util.TimerTask
 
-class MainActivity : ComponentActivity() {
-    private val DOWNLOAD_URL = "https://github.com/threethan/LightningLauncher/releases/"
-    var OPENED_WEB = false
+class MainActivity : Activity() {
+    private val downloadUrl = "https://github.com/threethan/LightningLauncher/releases/"
+    private var openedWeb = false
     private fun launch(): Boolean {
         var launchIntent = packageManager.getLaunchIntentForPackage("com.threethan.launcher")
         launchIntent?.let { return launchIt(it); }
@@ -17,10 +14,10 @@ class MainActivity : ComponentActivity() {
         launchIntent?.let { return launchIt(it); }
         launchIntent = packageManager.getLaunchIntentForPackage("com.veticia.piLauncherNext")
         launchIntent?.let { return launchIt(it); }
-        return false;
+        return false
     }
     private fun launchIt(launchIntent: Intent): Boolean {
-        launchIntent!!.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+        launchIntent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
         finish()
         startActivity(launchIntent)
         return true
@@ -28,11 +25,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (!launch() && !OPENED_WEB) {
-            OPENED_WEB = true
+        if (!launch() && !openedWeb) {
+            openedWeb = true
             val browserIntent = Intent(
                 Intent.ACTION_VIEW,
-                Uri.parse(DOWNLOAD_URL)
+                Uri.parse(downloadUrl)
             )
             startActivity(browserIntent)
         }
