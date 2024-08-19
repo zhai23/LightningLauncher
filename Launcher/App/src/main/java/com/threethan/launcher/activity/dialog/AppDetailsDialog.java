0fd1ab5aa48ea662eaf9513f8a65b7666dbe06f1
threethan <ethanjmedei@gmail.com>
2024-08-19 03:28:11 -0400

    Enable directly launching Quest Game Tuner

--- Launcher/App/src/main/java/com/threethan/launcher/activity/dialog/AppDetailsDialog.java
+++ Launcher/App/src/main/java/com/threethan/launcher/activity/dialog/AppDetailsDialog.java
@@ -20,6 +20,7 @@ import com.threethan.launcher.activity.LauncherActivity;
 import com.threethan.launcher.activity.support.SettingsManager;
 import com.threethan.launcher.data.Settings;
 import com.threethan.launcher.helper.AppExt;
+import com.threethan.launcher.helper.TunerLauncher;
 import com.threethan.launchercore.icon.IconLoader;
 import com.threethan.launchercore.icon.IconUpdater;
 import com.threethan.launchercore.lib.ImageLib;
@@ -60,18 +61,18 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
 
         PackageInfo packageInfo;
         try {
-            packageInfo = context.getPackageManager().getPackageInfo(app.packageName, 0);
+            packageInfo = a.getPackageManager().getPackageInfo(app.packageName, 0);
             ((TextView) dialog.findViewById(R.id.packageVersion)).setText(packageInfo.versionName);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 View pnv = dialog.findViewById(R.id.packageNameAndVersion);
-                pnv.setTooltipText(packageInfo.packageName + "\n" + packageInfo.versionName);
-                LcToolTipHelper.init(pnv, null);
+                LcToolTipHelper.init(pnv, packageInfo.packageName
+                        + "\n" + packageInfo.versionName);
             }
         } catch (PackageManager.NameNotFoundException ignored) {}
         // Info Action
         dialog.findViewById(R.id.info).setOnClickListener(view
-                -> AppExt.openInfo(context, app.packageName));
+                -> AppExt.openInfo(a, app.packageName));
         dialog.findViewById(R.id.uninstall).setOnClickListener(view -> {
             AppExt.uninstall(app.packageName); dialog.dismiss();});
 
@@ -96,10 +97,10 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
             customIconFile = IconLoader.iconCustomFileForApp(app);
             if (customIconFile.exists()) //noinspection ResultOfMethodCallIgnored
                 customIconFile.delete();
-            context.setSelectedIconImage(iconImageView);
+            a.setSelectedIconImage(iconImageView);
 
             imageApp = app;
-            context.showImagePicker(LauncherActivity.ImagePickerTarget.ICON);
+            a.showImagePicker(LauncherActivity.ImagePickerTarget.ICON);
         });
 
         App.Type appType = AppExt.getType(app);
@@ -109,12 +110,14 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
             IconLoader.cachedIcons.remove(app.packageName);
             IconUpdater.nextCheckByPackageMs.remove(app.packageName);
             IconLoader.loadIcon(app, d
-                    -> context.runOnUiThread(() -> iconImageView.setImageDrawable(d)));
+                    -> a.runOnUiThread(() -> iconImageView.setImageDrawable(d)));
         });
         if (appType == App.Type.VR || appType == App.Type.PANEL
                 || Platform.isTv()) {
             // VR apps MUST launch out, so just hide the option and replace it with another
             // Also hide it on TV where it is useless
+            tuningButton.setVisibility(View.VISIBLE);
+            tuningButton.setOnClickListener(v -> TunerLauncher.openForApp(app));
 
             refreshIconButton.setVisibility(View.VISIBLE);
             launchSizeSpinner.setVisibility(View.GONE);
@@ -126,7 +129,7 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
             // Browser selection spinner
             if (appType == App.Type.WEB) {
                 final String launchBrowserKey = Settings.KEY_LAUNCH_BROWSER + app.packageName;
-                final int launchBrowserSelection = context.dataStoreEditor.getInt(
+                final int launchBrowserSelection = a.dataStoreEditor.getInt(
                         launchBrowserKey,
                         SettingsManager.getAppLaunchOut(app.packageName) ? 0 : 1);
                 launchBrowserSpinner.setSelection(launchBrowserSelection);
@@ -134,16 +137,16 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
                         Platform.isQuest()
                                 ? R.array.advanced_launch_browsers_quest
                                 : R.array.advanced_launch_browsers,
-                        p -> context.dataStoreEditor.putInt(launchBrowserKey, p));
+                        p -> a.dataStoreEditor.putInt(launchBrowserKey, p));
                 launchBrowserSpinner.setVisibility(View.VISIBLE);
                 launchSizeSpinner.setVisibility(View.GONE);
             } else {
                 final String launchSizeKey = Settings.KEY_LAUNCH_SIZE + app.packageName;
-                final int launchSizeSelection = context.dataStoreEditor.getInt(
+                final int launchSizeSelection = a.dataStoreEditor.getInt(
                         launchSizeKey,
                         SettingsManager.getAppLaunchOut(app.packageName) ? 0 : 1);
                 initSpinner(launchSizeSpinner, R.array.advanced_launch_sizes, p ->
-                        context.dataStoreEditor.putInt(launchSizeKey, p)
+                        a.dataStoreEditor.putInt(launchSizeKey, p)
                 );
                 launchSizeSpinner.setSelection(launchSizeSelection);
                 launchSizeSpinner.setVisibility(View.VISIBLE);
@@ -170,26 +173,26 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
         showButton.setVisibility( amHidden ? View.VISIBLE : View.GONE);
         hideButton.setVisibility(!amHidden ? View.VISIBLE : View.GONE);
         showButton.setOnClickListener(v -> {
-            context.settingsManager.setAppGroup(app.packageName,
+            a.settingsManager.setAppGroup(app.packageName,
                     finalUnhideGroup);
             boolean nowHidden = Objects.equals(SettingsManager.getAppGroupMap()
                     .get(app.packageName), Settings.HIDDEN_GROUP);
             showButton.setVisibility( nowHidden ? View.VISIBLE : View.GONE);
             hideButton.setVisibility(!nowHidden ? View.VISIBLE : View.GONE);
-            BasicDialog.toast(context.getString(R.string.moved_shown), finalUnhideGroup, false);
-            context.launcherService.forEachActivity(LauncherActivity::refreshAppList);
+            BasicDialog.toast(a.getString(R.string.moved_shown), finalUnhideGroup, false);
+            a.launcherService.forEachActivity(LauncherActivity::refreshAppList);
 
         });
         hideButton.setOnClickListener(v -> {
-            context.settingsManager.setAppGroup(app.packageName,
+            a.settingsManager.setAppGroup(app.packageName,
                     Settings.HIDDEN_GROUP);
             boolean nowHidden = Objects.equals(SettingsManager.getAppGroupMap()
                     .get(app.packageName), Settings.HIDDEN_GROUP);
             showButton.setVisibility( nowHidden ? View.VISIBLE : View.GONE);
             hideButton.setVisibility(!nowHidden ? View.VISIBLE : View.GONE);
-            BasicDialog.toast(context.getString(R.string.moved_hidden),
-                    context.getString(R.string.moved_hidden_bold), false);
-            context.launcherService.forEachActivity(LauncherActivity::refreshAppList);
+            BasicDialog.toast(a.getString(R.string.moved_hidden),
+                    a.getString(R.string.moved_hidden_bold), false);
+            a.launcherService.forEachActivity(LauncherActivity::refreshAppList);
         });
 
         boolean isBanner = AppExt.isBanner(app);
@@ -199,22 +202,22 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
         dispBannerButton.setVisibility(!isBanner ? View.VISIBLE : View.GONE);
         dispIconButton.setOnClickListener(v -> {
             SettingsManager.setAppBannerOverride(app, false);
-            context.launcherService.forEachActivity(LauncherActivity::refreshAppList);
-            iconImageView.getLayoutParams().width = context.dp(83);
+            a.launcherService.forEachActivity(LauncherActivity::refreshAppList);
+            iconImageView.getLayoutParams().width = a.dp(83);
             refreshIconButton.callOnClick();
             dispBannerButton.setVisibility(View.VISIBLE);
             dispIconButton.setVisibility(View.GONE);
         });
         dispBannerButton.setOnClickListener(v -> {
             SettingsManager.setAppBannerOverride(app, true);
-            context.launcherService.forEachActivity(LauncherActivity::refreshAppList);
-            iconImageView.getLayoutParams().width = context.dp(150);
+            a.launcherService.forEachActivity(LauncherActivity::refreshAppList);
+            iconImageView.getLayoutParams().width = a.dp(150);
             refreshIconButton.callOnClick();
             dispBannerButton.setVisibility(View.GONE);
             dispIconButton.setVisibility(View.VISIBLE);
         });
         iconImageView.setClipToOutline(true);
-        iconImageView.getLayoutParams().width = context.dp(isBanner ? 150 : 83);
+        iconImageView.getLayoutParams().width = a.dp(isBanner ? 150 : 83);
 
         // Set Label (don't show star)
         String label = SettingsManager.getAppLabel(app);
@@ -234,7 +237,7 @@ public class AppDetailsDialog extends BasicDialog<LauncherActivity> {
             String newLabel = StringLib.setStarred(appNameEditText.getText().toString(), isStarred[0]);
             if (!newLabel.equals(SettingsManager.getAppLabel(app))) {
                 SettingsManager.setAppLabel(app, newLabel);
-                context.launcherService.forEachActivity(a -> {
+                a.launcherService.forEachActivity(a -> {
                     if (a.getAppAdapter() != null) {
                         a.getAppAdapter().notifyItemChanged(app);
                         a.refreshAppList();
---
+++ Launcher/App/src/main/java/com/threethan/launcher/helper/TunerLauncher.java
@@ -0,0 +1,39 @@
+package com.threethan.launcher.helper;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+
+import com.threethan.launchercore.Core;
+import com.threethan.launchercore.util.App;
+
+/**
+ * Allows direct opening of Quest Game Tuner tuning settings for a specific app
+ */
+public class TunerLauncher {
+    private static final String APP_TUNING_URI = "tune_app";
+
+    /** Opens Quest Game Tuner for a given app */
+    public static void openForApp(ApplicationInfo app) {
+        openForPackage(app.packageName);
+    }
+    /** Opens Quest Game Tuner for a given package */
+    public static void openForPackage(String packageName) {
+        if (App.packageExists("com.threethan.tuner")) {
+            if (packageName == null || packageName.isEmpty()) return;
+            Intent intent = new Intent();
+            intent.setPackage("com.threethan.tuner");
+            intent.setComponent(new ComponentName("com.threethan.tuner", "com.threethan.tuner.dialog.DialogActivity"));
+            intent.setAction("com.threethan.tuning.DIALOG");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse(APP_TUNING_URI));
+            intent.putExtra(Intent.EXTRA_PACKAGE_NAME, packageName);
+            Core.context().startActivity(intent);
+        } else {
+            Intent openURL = new Intent(Intent.ACTION_VIEW);
+            openURL.setData(Uri.parse("https://threethan.itch.io/quest-game-tuner"));
+            Core.context().startActivity(openURL);
+        }
+    }
+}
