02559e7f9a4246b7475b69f71080fbe0b99139a5
threethan <ethanjmedei@gmail.com>
2024-10-15 01:40:50 -0400

    Fix library shortcut service

--- RedirectServices/app/src/main/java/com/threethan/launcher/service/ShortcutAccessibilityService.java
+++ RedirectServices/app/src/main/java/com/threethan/launcher/service/ShortcutAccessibilityService.java
@@ -10,25 +10,32 @@ import java.util.TimerTask;
 
 public class ShortcutAccessibilityService extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        String eventText = event.getText().toString().toLowerCase();
+
+        if (!event.getPackageName().equals("com.oculus.systemux")) return;
+
+        String eventText = event.getText().toString();
+
         String targetName = getResources().getString(R.string.target_name);
-        String targetEventName = "["+targetName+"]";
 
-
-        if (targetEventName.toLowerCase().compareTo(eventText.toLowerCase()) == 0) {
-            Intent launchIntent = new Intent(this, MainActivity.class);
-            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-
-            Log.i("LightningLauncherService", "Opening launcher activity from accessibility event");
-            startActivity(launchIntent);
-            new Timer().schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    startActivity(launchIntent);
-                }
-            }, 650);
+        if (eventText.compareTo("[]") == 0) {
+            if (event.getContentDescription() == null) return;
+            if (targetName.compareToIgnoreCase(event.getContentDescription().toString()) != 0) return;
+        } else {
+            String targetEventName = "[" + targetName + "]";
+            if (targetEventName.toLowerCase().compareToIgnoreCase(eventText) != 0) return;
         }
 
+        Intent launchIntent = new Intent(this, MainActivity.class);
+        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+        Log.i("LightningLauncherService", "Opening launcher activity from accessibility event");
+        startActivity(launchIntent);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(launchIntent);
+            }
+        }, 650);
     }
 
     public void onInterrupt() {}
