895d4385ec97a5d53049c2fef6afae0f9cd12283
threethan <ethanjmedei@gmail.com>
2024-10-24 02:37:14 -0400

    HOTFIX: Roll back activity changes, blur by default

--- Launcher/App/src/main/java/com/threethan/launcher/activity/LauncherActivityBlurred.java
+++
@@ -1,4 +0,0 @@
-package com.threethan.launcher.activity;
-
-/** Used as a separate manifest entry to enable compositing blur on Quest 3 */
-public class LauncherActivityBlurred extends LauncherActivitySearchable { }
