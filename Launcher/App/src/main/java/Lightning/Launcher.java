d1acf684314490273570f9b72140243b8769f2d3
threethan <ethanjmedei@gmail.com>
2025-03-24 02:01:09 -0400

    Targets for Q1 (v50)

---
+++ Launcher/App/src/main/java/Lightning/Launcher.java
@@ -0,0 +1,7 @@
+package Lightning;
+
+import com.threethan.launcher.activity.LauncherActivitySearchable;
+
+/** Quest OS displays the activity classpath in the app list.
+ *  This activity and package exist for cosmetic reasons only. */
+public class Launcher extends LauncherActivitySearchable {}
